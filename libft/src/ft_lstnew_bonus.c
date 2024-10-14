4c002b97d301b57607a13a957d5a023403ed5d9f
Anna Ilchenko <anilchen@c1r6s2.42wolfsburg.de>
2024-10-14 16:47:31 +0200

    changes

--- libft/src/ft_lstnew_bonus.c
+++ libft/src/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: anilchen <anilchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 17:19:22 by anilchen          #+#    #+#             */
-/*   Updated: 2024/07/10 13:26:18 by anilchen         ###   ########.fr       */
+/*   Updated: 2024/10/14 16:47:00 by anilchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ t_list	*ft_lstnew(void *content)
 	return (node);
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int	main(void)
-{
-	int value = 42;
-	t_list *node = ft_lstnew(&value); // Создание узла с указателем на int
+// int	main(void)
+// {
+// 	int value = 42;
+// 	t_list *node = ft_lstnew(&value); // Создание узла с указателем на int
 
-	// Пример вывода значения content узла
-	printf("Value stored in node: %d\n", *(int *)(node->content));
+// 	// Пример вывода значения content узла
+// 	printf("Value stored in node: %d\n", *(int *)(node->content));
 
-	free(node); // Освобождение памяти
-	return (0);
-}
+// 	free(node); // Освобождение памяти
+// 	return (0);
+// }
